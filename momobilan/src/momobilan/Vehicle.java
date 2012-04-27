4e12d1845b98991c7eb2274c7c89ca04c986df92
Andhika Nugraha <andhika.nugraha@gmail.com>
2012-04-27 23:46:08 +0700

    Melengkapi kelas Vehicle

--- momobilan/src/momobilan/Vehicle.java
+++ momobilan/src/momobilan/Vehicle.java
@@ -4,6 +4,7 @@
  */
 package momobilan;
 
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
@@ -16,13 +17,30 @@ public class Vehicle extends Thread {
     public int newLane;
     public int newDistance;
     
-    protected Track parentTrack;
-    protected int interval = 500;
+    private Track parentTrack;
+    private volatile boolean isActive = false;
     
-    private boolean isActive = false;
+    /**
+     * Kecepatan mobil ditentukan oleh dua properti:
+     * - velocity: perpindahan setiap interval
+     * - interval: jangka waktu antara perpindahan
+     */
+    private int velocity;
+    private int interval = 1000;
+    
+    private float acceleration = 0;
     
     private ConcurrentLinkedQueue<TrackListener> listeners;
+    
+    private HashMap<String,String> attributes;
 
+    /**
+     * Menghubungkan mobil dengan dunia trek
+     * 
+     * @param track
+     * @param lane
+     * @param distance 
+     */
     public void attach(Track track, int lane, int distance) {
         parentTrack = track;
         currentLane = lane;
@@ -31,25 +49,100 @@ public class Vehicle extends Thread {
         start();
     }
     
-    protected void move() {
+    /**
+     * Pergerakan default
+     * 
+     * Tanpa intervensi dari luar, mobil akan bergerak sesuai dengan
+     * properti velocity dan interval.
+     */
+    protected void defaultMotion() throws InterruptedException {
         // Lakukan sebuah pergerakan
-        // I.S. Terdefinisi currentLane, currentDistance
-        // F.S. Terdefinisi newLane, newDistance.
+        sleep(interval);
+
+        newDistance = currentDistance + velocity;
+        velocity += acceleration * velocity;
+        
+        updateTrack();
     }
     
+    /**
+     * Berpindah posisi
+     * 
+     * @param deltaLane
+     * @param deltaDistance 
+     */
+    public void move(int deltaLane, int deltaDistance) {
+        newLane = currentLane + deltaLane;
+        newDistance = currentDistance + deltaDistance;
+        
+        updateTrack();
+    }
+    
+    /**
+     * Berpindah lajur.
+     * Note: Pemanggilan event hasil pemindahan lajur
+     *       hanya akan dipanggil dalam iterasi run().
+     * @param deltaLane 
+     */
+    public void changeLane(int deltaLane) {
+        newLane = currentLane + deltaLane;
+        
+        updateTrack();
+    }
+    
+    /**
+     * Mengubah kecepatan mobil berdasarkan perpindahan per selang waktu
+     * 
+     * @param velocity 
+     */
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+    
+    /**
+     * Mengubah kecepatan mobil berdasarkan selang waktu per perpindahan
+     * 
+     * @param interval 
+     */
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+    
+    /**
+     * Mengubah kecepatan mobil berdasarkan perpindahan dan selang waktu
+     * 
+     * @param velocity
+     * @param interval 
+     */
+    public void setVelocity(int velocity, int interval) {
+        this.velocity = velocity;
+        this.interval = interval;
+    }
+    
+    /**
+     * Memperbarui kondisi track di mana mobil berada
+     * 
+     * I.S. parentTrack terdefinisi
+     * F.S. Terdaftar perpindahan pada parentTrack, serta event dipanggil.
+     */
+    protected void updateTrack() {
+        parentTrack.registerMovement((int) currentLane, (int) currentDistance, (int) newLane, (int) newDistance);
+
+        currentLane = newLane;
+        currentDistance = newDistance;
+    }
+    
+    /**
+     * Konkurensi
+     * 
+     * Secara default, mobil akan bergerak (bermutasi) sesuai defaultMotion()
+     */
     @Override
     public void run() {
         try {
             while (isActive) {
                 // do stuff
-                Thread.sleep(interval);
-                
-                move();
-                
-                parentTrack.registerMovement(currentLane, currentDistance, newLane, newDistance);
-                
-                currentLane = newLane;
-                currentDistance = newDistance;
+                defaultMotion();
             }
         }
         catch (InterruptedException e) {
@@ -57,6 +150,9 @@ public class Vehicle extends Thread {
         }
     }
     
+    /**
+     * Dipanggil setelah terjadi perpindahan yang aman.
+     */
     public void hasMoved() {
         // Tidak terjadi tabrakan.
         
@@ -66,6 +162,9 @@ public class Vehicle extends Thread {
         }
     }
     
+    /**
+     * Menghilangkan keberadaan dan pergerakan mobil.
+     */
     public void die() {
         isActive = false;
         
