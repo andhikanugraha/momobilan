ac75aea14b01463f9b5a3fdd57e8240018d2a077
Abdurrosyid Broto Handoyo <arrosyidbh@gmail.com>
2012-04-28 12:56:46 +0700

    Coba-coba commit

--- momobilan/src/momobilan/Momobilan.java
+++ momobilan/src/momobilan/Momobilan.java
@@ -14,6 +14,7 @@ public class Momobilan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello World!!");
         // TODO code application logic here
     }
 }
